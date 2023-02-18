4fbda4a7c699853d0a1b1da2ccb667c61e66a73b
Vi <96641937+HuynhLyMinhVi@users.noreply.github.com>
2023-02-18 17:46:08 +0700

    Liststore Basc 50%

--- src/Components/StoreManager/ListStore/ListStore.js
+++ src/Components/StoreManager/ListStore/ListStore.js
@@ -1,10 +1,44 @@
 import React from "react";
+import "./liststore.css";
 
 const ListStore = () => {
   return (
-    <div>
-      Hello hello chi danh sach
-    </div>
+    <>
+      <div className="liststore">
+        <h1>Danh sách cửa hàng</h1>
+        <div className="liststorebody">
+          {/* Button  Add */}
+          <button className="addliststore">Thêm mới cửa hàng</button>
+          {/* Status */}
+          <div className="status">
+            <span>Tình Trạng</span>
+            {/* From status  */}
+            <div className="fromstatus">
+              <input name="gender" type="radio" value="Tất cả" />
+              <p>Tất cả</p>
+              <input name="gender" type="radio" value="Đang hoạt động" />
+              <p>Đang hoạt động</p>
+              <input name="gender" type="radio" value="Đã tạm dừng" />
+              <p>Đã tạm dừng</p>
+            </div>
+            {/* Search */}
+            <div className="searchinput">
+              <input
+                type="text"
+                class="searchTerm"
+                placeholder="Tìm kiếm..."
+              ></input>
+            </div>
+            {/* Button Search */}
+            <span className="buttonsearch">
+              <button>Tìm Kiếm</button>
+            </span>
+          </div>
+        </div>
+        {/* Table Store */}
+        <div className="table">helo</div>
+      </div>
+    </>
   );
 };
 
