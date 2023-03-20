af1866a08241585d3edfbc41389a774d51534d2e
HuynhVi <huynhlyminhvi@gmail.com>
2023-03-20 11:47:00 +0700

    Up date details trên nhánh hai-client

--- src/Components/DetailsPawn/DetailsPawn.js
+++ src/Components/DetailsPawn/DetailsPawn.js
@@ -5,23 +5,36 @@ import ContentPawn from "./ContentPawn";
 import HeaderPawn from "./HeaderPawn";
 import TablePawn from "./TablePawn";
 import AddContract from './PopUp/AddContract';
+import UpdateContract from "./PopUp/UpdateContract";
+import Liquidation from "./PopUp/Liquidation";
 const DetailsPawn = () => {
 const [showAddContract,setShowAddContract] = useState(false)
+const [showUpdateContract,setShowUpdateContract] = useState(false)
+const [showliquidation,setShowliquidation] = useState(false)
+
+
 
   return (
    
       <div className="details-pawn">
         <div>
-          <HeaderPawn setShowAddContract={setShowAddContract} />
+          <HeaderPawn setShowAddContract={setShowAddContract}/>
         </div>
         <div>
           <ContentPawn />
           <BtnDetails />
         </div>
-      <TablePawn/>
+      <TablePawn setShowUpdateContract={setShowUpdateContract} setShowliquidation={setShowliquidation}/>
       {
         showAddContract && <AddContract setShowAddContract={setShowAddContract}/>
       }
+      {
+        showUpdateContract && <UpdateContract setShowUpdateContract={setShowUpdateContract}/>
+      }
+      {
+        showliquidation && <Liquidation setShowliquidation={setShowliquidation}/>
+      }
+      
       </div>
       
   );
--- src/routes/index.js
+++ src/routes/index.js
@@ -18,7 +18,7 @@ import Home from "./../Components/Home/Home";
 
 const publicRoutes = [
     {
-        path: "/home",
+        path: "/",
         component: Home,
     },
     {
