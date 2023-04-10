388a8d8967e8500aa07ad09c8c95dcf6f490ac70
Harry <96876113+Thanhhaidev1905@users.noreply.github.com>
2023-04-11 00:10:47 +0700

    fix ui add new customer

--- src/Components/CustomerManager/Popup/AddNewCustomer.js
+++ src/Components/CustomerManager/Popup/AddNewCustomer.js
@@ -45,19 +45,12 @@ const AddNewCustomer = () => {
         },
     };
 
-    const [img, setImg] = useState([]);
-    const [fullname, setFullName] = useState('');
-    const [cccd, setCccd] = useState('');
-    const [phone, setPhone] = useState('');
-    const [address, setAddress] = useState('');
-    const [faceImg, setFaceImg] = useState('');
-
     // API upload img
     function uploadCusImg(customerImg) {
         setFaceImg(customerImg);
     }
     const onAddNewCustomer = () => {
-        console.log('fullname', fullname);
+        console.log('fullname', name);
         // alert(faceImg);
         // API({
         //     method: 'post',
@@ -81,10 +74,17 @@ const AddNewCustomer = () => {
         //         alert('Tạo KH fail');
         //     });
     };
-    const handleName = (event) => {
-        setFullName(event.target.value);
+    const [img, setImg] = useState([]);
+    const [name, setName] = useState('');
+    const [cccd, setCccd] = useState('');
+    const [phone, setPhone] = useState('');
+    const [address, setAddress] = useState('');
+    const [faceImg, setFaceImg] = useState('');
 
-        console.log('value is:', event.target.value);
+    const handleName = (e) => {
+        setName(e.target.value);
+
+        console.log('value is:', e.target.value);
     };
     const handleCccd = (e) => {
         setCccd(e.target.value);
@@ -100,63 +100,66 @@ const AddNewCustomer = () => {
         <div className="headerCustomer">
             <h1 className="headerCustomerName">Thêm Mới Khách Hàng</h1>
             <div>
-                <Item className="parperCustomer">
+                <div className="parperCustomer">
                     <div className="infoCustomer">
-                        <div className="userInfo">
-                            <div className="userInfoLabel">
-                                <p>
-                                    Tên khách hàng <span class="starRed">*</span>:
-                                </p>
-                                <p>
-                                    Số CMND/Hộ chiếu:<span class="starRed">*</span>:
-                                </p>
-                                <p>
-                                    Số điện thoại <span class="starRed">*</span>:
-                                </p>
-                                <p>
-                                    Địa chỉ <span class="starRed">*</span>:
-                                </p>
-                                <p>
-                                    Chứng từ <span class="starRed">*</span>:
-                                </p>
-                            </div>
-                            <div className="userInfoInput">
-                                <input
-                                    type="text"
-                                    placeholder="Nhập tên khách hàng..."
-                                    name="fullname"
-                                    value={fullname}
-                                    onchange={handleName}
-                                />
-                                <input
-                                    type="text"
-                                    placeholder="Nhập CMND/Hộ chiếu..."
-                                    onchange={(e) => setCccd(e.target.value)}
-                                    value={cccd}
-                                />
-                                <input
-                                    type="text"
-                                    placeholder="Nhập số điện thoại..."
-                                    onchange={handlePhone}
-                                    // value={phone}
-                                />
-                                <input
-                                    type="text"
-                                    placeholder="Nhập địa chỉ..."
-                                    onchange={handleAddress}
-                                    // value={address}
-                                />
-                                <div className="chungtu">
-                                    <UploadButton
-                                        uploader={uploader}
-                                        options={{ multi: true }}
-                                        onComplete={(files) => uploadCusImg(files.map((x) => x.fileUrl).join('\n'))}
-                                    >
-                                        {({ onClick }) => <button onClick={onClick}>Upload a file...</button>}
-                                    </UploadButton>
+                        <form>
+                            <div className="userInfo">
+                                <div className="userInfoLabel">
+                                    <p>
+                                        Tên khách hàng <span class="starRed">*</span>:
+                                    </p>
+                                    <p>
+                                        Số CMND/Hộ chiếu:<span class="starRed">*</span>:
+                                    </p>
+                                    <p>
+                                        Số điện thoại <span class="starRed">*</span>:
+                                    </p>
+                                    <p>
+                                        Địa chỉ <span class="starRed">*</span>:
+                                    </p>
+                                    <p>
+                                        Chứng từ <span class="starRed">*</span>:
+                                    </p>
+                                </div>
+                                <div className="userInfoInput">
+                                    <input
+                                        type="text"
+                                        placeholder="Nhập tên khách hàng..."
+                                        // id="fullname"
+                                        // name="fullname"
+                                        value={name}
+                                        onChange={(e) => handleName(e)}
+                                    />
+                                    <input
+                                        type="text"
+                                        placeholder="Nhập CMND/Hộ chiếu..."
+                                        value={cccd}
+                                        onChange={(e) => setCccd(e.target.value)}
+                                    />
+                                    <input
+                                        type="text"
+                                        placeholder="Nhập số điện thoại..."
+                                        onChange={handlePhone}
+                                        // value={phone}
+                                    />
+                                    <input
+                                        type="text"
+                                        placeholder="Nhập địa chỉ..."
+                                        onChange={handleAddress}
+                                        // value={address}
+                                    />
+                                    <div className="chungtu">
+                                        <UploadButton
+                                            uploader={uploader}
+                                            options={{ multi: true }}
+                                            onComplete={(files) => uploadCusImg(files.map((x) => x.fileUrl).join('\n'))}
+                                        >
+                                            {({ onClick }) => <button onClick={onClick}>Upload a file...</button>}
+                                        </UploadButton>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </form>
                         <div className="comfirmBtn">
                             <button className="saveBtn" onClick={onAddNewCustomer}>
                                 <img src={saveBtn} alt="" />
@@ -170,7 +173,7 @@ const AddNewCustomer = () => {
                             </Link>
                         </div>
                     </div>
-                </Item>
+                </div>
             </div>
         </div>
     );
