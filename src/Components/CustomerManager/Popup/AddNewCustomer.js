4ccdd1528dd0d881788a45eb0fba89e3b3020181
Harry <96876113+Thanhhaidev1905@users.noreply.github.com>
2023-04-10 22:17:55 +0700

    up test

--- src/Components/CustomerManager/Popup/AddNewCustomer.js
+++ src/Components/CustomerManager/Popup/AddNewCustomer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { styled } from '@mui/material/styles';
 import './UpdateInfor.css';
@@ -7,6 +7,9 @@ import Paper from '@mui/material/Paper';
 import saveBtn from '../../../asset/img/save1.png';
 import returnBtn from '../../../asset/img/returnBTN.png';
 import { Link } from 'react-router-dom';
+import { Uploader } from 'uploader';
+import { UploadButton, UploadDropzone } from 'react-uploader';
+import API from '../../../API';
 
 const AddNewCustomer = () => {
     const Item = styled(Paper)(({ theme }) => ({
@@ -25,9 +28,77 @@ const AddNewCustomer = () => {
         return <input type="file" />;
     };
 
+    // upload img
+
+    const uploader = Uploader({ apiKey: 'public_W142hmnA4cUzeNeYbEgGT17DUUbE' }); // Your real API key.
+    const uploaderOptions = {
+        multi: true,
+
+        // Comment out this line & use 'onUpdate' instead of
+        // 'onComplete' to have the dropzone close after upload.
+        showFinishButton: true,
+
+        styles: {
+            colors: {
+                primary: '#377dff',
+            },
+        },
+    };
+
+    const [img, setImg] = useState([]);
+    const [fullname, setFullName] = useState('');
+    const [cccd, setCccd] = useState('');
+    const [phone, setPhone] = useState('');
+    const [address, setAddress] = useState('');
+    const [faceImg, setFaceImg] = useState('');
+
+    // API upload img
+    function uploadCusImg(customerImg) {
+        setFaceImg(customerImg);
+    }
+    const onAddNewCustomer = () => {
+        console.log('fullname', fullname);
+        // alert(faceImg);
+        // API({
+        //     method: 'post',
+        //     url: '/customer/createCustomer',
+        //     data: {
+        //         // "branchId": id,
+        //         fullName: fullname,
+        //         cccd: cccd,
+        //         phone: phone,
+        //         address: address,
+        //         faceImg: faceImg,
+        //         status: 1,
+        //         point: 100,
+        //     },
+        // })
+        //     .then((res) => {
+        //         alert('Tạo KH thành công');
+        //     })
+        //     .catch((err) => {
+        //         console.log(err);
+        //         alert('Tạo KH fail');
+        //     });
+    };
+    const handleName = (event) => {
+        setFullName(event.target.value);
+
+        console.log('value is:', event.target.value);
+    };
+    const handleCccd = (e) => {
+        setCccd(e.target.value);
+    };
+    const handlePhone = (e) => {
+        setPhone(e.target.value);
+    };
+    const handleAddress = (e) => {
+        setAddress(e.target.value);
+    };
+
     return (
         <div className="headerCustomer">
-            <h1 className="headerCustomerName">Thêm Khách Hàng</h1>
+            <h1 className="headerCustomerName">Thêm Mới Khách Hàng</h1>
             <div>
                 <Item className="parperCustomer">
                     <div className="infoCustomer">
@@ -50,17 +121,44 @@ const AddNewCustomer = () => {
                                 </p>
                             </div>
                             <div className="userInfoInput">
-                                <input type="text" placeholder="Nhập tên khách hàng..." />
-                                <input type="text" placeholder="Nhập CMND/Hộ chiếu..." />
-                                <input type="text" placeholder="Nhập số điện thoại..." />
-                                <input type="text" placeholder="Nhập địa chỉ..." />
+                                <input
+                                    type="text"
+                                    placeholder="Nhập tên khách hàng..."
+                                    name="fullname"
+                                    value={fullname}
+                                    onchange={handleName}
+                                />
+                                <input
+                                    type="text"
+                                    placeholder="Nhập CMND/Hộ chiếu..."
+                                    onchange={(e) => setCccd(e.target.value)}
+                                    value={cccd}
+                                />
+                                <input
+                                    type="text"
+                                    placeholder="Nhập số điện thoại..."
+                                    onchange={handlePhone}
+                                    // value={phone}
+                                />
+                                <input
+                                    type="text"
+                                    placeholder="Nhập địa chỉ..."
+                                    onchange={handleAddress}
+                                    // value={address}
+                                />
                                 <div className="chungtu">
-                                    <button onClick={onHandleNewFile}>Thêm mới</button>
+                                    <UploadButton
+                                        uploader={uploader}
+                                        options={{ multi: true }}
+                                        onComplete={(files) => uploadCusImg(files.map((x) => x.fileUrl).join('\n'))}
+                                    >
+                                        {({ onClick }) => <button onClick={onClick}>Upload a file...</button>}
+                                    </UploadButton>
                                 </div>
                             </div>
                         </div>
                         <div className="comfirmBtn">
-                            <button className="saveBtn">
+                            <button className="saveBtn" onClick={onAddNewCustomer}>
                                 <img src={saveBtn} alt="" />
                                 Lưu lại
                             </button>
