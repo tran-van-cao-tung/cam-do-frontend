38453776e59cb8b2db5ea17819a90fcee4169eae
HuynhVi <huynhlyminhvi@gmail.com>
2023-04-30 22:14:40 +0700

    fix url

--- src/API.js
+++ src/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const callAPI = axios.create({
-    baseURL: 'https://tranvancaotung1-001-site1.htempurl.com/api/v1',
+    baseURL: 'http://tranvancaotung2-001-site1.etempurl.com/api/v1',
     headers: {
         Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
     },
