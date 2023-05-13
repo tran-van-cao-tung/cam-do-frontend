c5e5142beeac1ec2702f7f184013e59fcae694f4
Tam Nguyen <67701889+TamNT62752@users.noreply.github.com>
2023-05-13 14:17:30 +0700

    fix tien phat

--- src/Components/DetailsPawn/PopUp/Ransom.js
+++ src/Components/DetailsPawn/PopUp/Ransom.js
@@ -132,7 +132,7 @@ const Ransom = ({ showContractId, contract, showdetailContract, setshowdetailCon
                     </Grid>
                     <Grid item xs={3} sx={{ textAlign: 'left' }}>
                         <Item sx={{ textAlign: 'center', fontSize: '25px', color: '#107287' }}>
-                            {ransomDetail.paidMoney ? formatMoney(ransomDetail.paidMoney) : '0 VNĐ'}
+                            {ransomDetail.penalty ? formatMoney(ransomDetail.penalty) : '0 VNĐ'}
                         </Item>
                     </Grid>
                 </Grid>
